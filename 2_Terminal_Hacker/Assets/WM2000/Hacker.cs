7728ea01b5492040449d05777698d0385e357a95
stevedixon90 <steveveve750@gmail.com>
2021-04-30 13:35:02 +0100

    Terminal hacker - drive fail safe

---
+++ 2_Terminal_Hacker/Assets/WM2000/Hacker.cs
@@ -0,0 +1,171 @@
+﻿using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+    // Game configuration data
+    const string menuHint = "You may type menu at any time.";
+    string[] level1Passwords = { "Books", "Aisle", "Shelf", "Font", "Borrow" };
+    string[] level2Passwords = { "Prisoner", "Handcuffs", "Holster", "Uniform", "Arrest" };
+    string[] level3Passwords = { "Conical", "Bunsen", "Constellation", "Supernova", "Blackhole" };
+
+    // Game state
+    int level;
+    enum Screen { MainMenu, Password, Win };
+    Screen currentScreen;
+    string password;
+
+    // Use this for initialization
+    void Start()
+    {
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu()
+    {
+        currentScreen = Screen.MainMenu;
+        Terminal.ClearScreen();
+        Terminal.WriteLine("What would you like to hack into?");
+        Terminal.WriteLine("Press 1 for the Library");
+        Terminal.WriteLine("Press 2 for the Police Station");
+        Terminal.WriteLine("Press 3 for the Laboratory");
+        Terminal.WriteLine("Enter your selection: ");
+    }
+
+    void OnUserInput(string input)
+    {
+        if (input == "menu")
+        {
+            ShowMainMenu();
+        }
+        else if (input == "quit" || input == "close" || input == "exit")
+        {
+
+        }
+        else if (currentScreen == Screen.MainMenu)
+        {
+            RunMainMenu(input);
+        }
+        else if (currentScreen == Screen.Password)
+        {
+            CheckPassword(input);
+        }
+    }
+
+    void RunMainMenu(string input)
+    {
+        bool isValidLevelNumber = (input == "1" || input == "2" || input == "3");
+        if (isValidLevelNumber)
+        {
+            level = int.Parse(input);
+            AskForPassword();
+        }
+        else if (input == "007")
+        {
+            Terminal.WriteLine("Hew james areet");
+        }
+        else
+        {
+            Terminal.WriteLine("Please choose a valid level");
+            Terminal.WriteLine(menuHint);
+        }
+    }
+
+    void AskForPassword()
+    {
+        currentScreen = Screen.Password;
+        Terminal.ClearScreen();
+        SetRandomPassword();
+        Terminal.WriteLine("Please enter your password, hint: " + password.Anagram());
+        Terminal.WriteLine(menuHint);
+    }
+
+    void SetRandomPassword()
+    {
+        switch (level)
+        {
+            case 1:
+                password = level1Passwords[Random.Range(0, level1Passwords.Length)];
+                break;
+            case 2:
+                password = level2Passwords[Random.Range(0, level2Passwords.Length)];
+                break;
+            case 3:
+                password = level3Passwords[Random.Range(0, level3Passwords.Length)];
+                break;
+            default:
+                Debug.LogError("Invalid level number");
+                break;
+        }
+    }
+
+    void CheckPassword(string input)
+    {
+        if (input == password)
+        {
+            DisplayWinScreen();
+        }
+        else
+        {
+            AskForPassword();
+        }
+    }
+
+    void DisplayWinScreen()
+    {
+        currentScreen = Screen.Win;
+        Terminal.ClearScreen();
+        ShowLevelReward();
+        Terminal.WriteLine(menuHint);
+    }
+
+    void ShowLevelReward()
+    {
+        switch (level)
+        {
+            case 1:
+                Terminal.WriteLine("Have a book...");
+                Terminal.WriteLine(@"
+    ________
+   /       //
+  /       //
+ /______ //
+(_______(/
+
+"
+                );
+                break;
+
+            case 2:
+                Terminal.WriteLine("Have a gun comrade!");
+                Terminal.WriteLine(@"
+   _________________-
+ +/   ______________||
+ /   /___/
+/__/ 
+
+"
+                );
+                Terminal.WriteLine("Play again for a greater challenge...");
+                break;
+
+            case 3:
+                Terminal.WriteLine("Here's a sample, Gordon... ");
+                Terminal.WriteLine(@"
+           = ++ -
+            +=++
+             =+
+            | |
+            | |
+           /   \
+          /     \
+         /_______\
+"
+                );
+                Terminal.WriteLine("... We hope for more resonance cascades Mr. Freeman...");
+                break;
+            default:
+                Debug.LogError("Invalid level reached");
+                break;
+        }
+    }
+}
